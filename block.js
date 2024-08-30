da029b2e92c5fc11dccb55f166add8e375087e64
Viktor7874 <viktorromme247@gmail.com>
2024-08-30 08:42:26 +0000

    Klasse Block/constructor,toString(),genesis()

---
+++ block.js
@@ -0,0 +1,30 @@
+class Block{
+
+    constructor(timestamp,lastHash,hash,data){
+        this.timestamp = timestamp; // Zeitstempel
+        this.lastHash = lastHash; // Hash des vorhergehenden Blocks
+        this.hash = hash; // Eigener Hash
+        this.data = data; // Daten
+    }
+
+  
+   toString(){ // für Debugging-Zwecke
+        return `Block -
+        Timestamp:  ${this.timestamp}
+        Last Hash:  ${this.lastHash}
+        Hash:       ${this.hash}
+        Data:       ${this.data}`
+    }
+
+    static genesis(){ // 1. Block der chain
+        return new this("Genesis time","-----","xABc1234",["data1","data2,data3"]); // call constructor
+    }
+}
+
+
+
+
+module.exports = Block; // Export als Modul
+
+
+
---
+++ dev-test.js
@@ -0,0 +1,9 @@
+
+// Import
+const Block = require("./block");
+
+/***  Test der Block-Funktionalität  */
+// const block = new Block("aktuelle zeit","hash1","hash2","Blockdaten");
+// console.log(block.toString());
+
+console.log(Block.genesis().toString());
