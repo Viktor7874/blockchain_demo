d740c47aa1d698ac1fd2cfa65cc6274473eb6364
Viktor7874 <viktorromme247@gmail.com>
2024-08-30 10:55:53 +0000

    02

--- dev-test.js
+++ dev-test.js
@@ -1,6 +1,7 @@
 
 // Import
 const Block = require("./block");
+const Blockchain = require("./blockchain");
 
 /***  Test der Block-Funktionalität  */
 // const block = new Block("aktuelle zeit","hash1","hash2","Blockdaten");
@@ -8,5 +9,12 @@ const Block = require("./block");
 
 // console.log(Block.genesis().toString());
 
-const testBlock = Block.mineBlock(Block.genesis(),"testDaten")
-console.log(testBlock.toString());
+// const testBlock = Block.mineBlock(Block.genesis(),"testDaten")
+// console.log(testBlock.toString());
+
+
+
+/***  Test der Blockchain-Funktionalität  */
+const chain = new Blockchain();
+console.log(chain);
+
